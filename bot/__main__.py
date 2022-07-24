26616a0cd68e0c8749baebe492f6a7b59f93480f
Shadow King <105120735+MasterShad0w@users.noreply.github.com>
2022-07-24 16:09:26 +0530

    Add files via upload

---
+++ bot/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @ShadowKing9o
+
+from .bot import Bot
+
+app = Bot()
+app.run()
---
+++ bot/translation.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @ShadowKing9o
+
+class Translation(object):
+    
+    START_TEXT = """<b>Hey {}!!</b>
+<b>Am Just <b><u>An Advance Auto Filter Bot....😉</u></b>
+
+Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+
+For More Details Click Help Button Below..
+@ShadowsArena
+</b>"""    
+    
+    HELP_TEXT = """
+<b><i><u>How To Use Me!?</u></i></b>
+
+<i>
+1.Add Me To Any Group And Make Me Admin
+2.Add Me To Files Database Channel
+</i>
+
+<b><u>Bot Commands</u>(Works Only In Groups) :</b>
+
+1.<code>/add chat_id</code> <b>OR</b> <code>/add @Username</code>
+ -<i>To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel.</i>
+
+2.<code>/del chat_id</code> <b>OR</b> <code>/del @Username</code> 
+ -<i>To disconnect A Group With A Channel.</i>
+     
+3.<code>/delall</code>
+ -<i>This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB.</i>
+    
+4.<code>/settings</code>
+ -<i>This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly.</i>
+
+<b><u>Bot Buttons</u>:</b>
+
+1.<b>Channel</b> 
+ -Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+            
+2.<b>Filter Types</b>
+ -Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+
+3.<b>Configure</b>
+ -Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            
+4.<b>Status</b>
+ -Button Will Shows The Stats Of Your Channel
+            
+@ShadowsArena
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <b> Friday Filter Bot</b>
+    
+<b>➥ Creator</b> : <b><i><a href="https://t.me/ShadowKing9o">🥷Shadow King🥷</a></i></b>
+
+<b>➥ Language</b> : <b>Python3</b>
+
+<b>➥ Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
+
+<b>➥ Source Code</b> : <b><a href="https://github.com/MasterShad0w/FridayFilterBot">F.R.I.D.A.Y Filter Bot</a></b>
+"""
